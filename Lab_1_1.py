b7fcc7b6ea1541b85cdd541969b13ea1aa3c9bea
Vasil112 <105806255+Vasil112@users.noreply.github.com>
2025-03-08 13:09:37 +0200

    Create

---
+++ Lab_1_1.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import simpledialog
+
+
+def show_dialog(): 
+    root = tk.Tk()
+    root.geometry("200x200")
+    root.resizable(False, False)
+    root.withdraw()
+    user_input = simpledialog.askstring("Введення", "Введіть текст:") 
+    if user_input is not None:
+        print("Ви ввели:", user_input) 
+
+show_dialog()
